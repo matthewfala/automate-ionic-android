db8b40ad8b766f1ef4ef52ff4a9833b76dc07609
matthewfala <matthewfala@gmail.com>
2020-05-26 23:53:03 -1000

    automate android emu deploy and app load for vscode ionic debugging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+         IonList, IonItem, IonCheckbox, IonLabel, IonBadge, IonNote,
+         IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import { RouteComponentProps } from 'react-router';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+const Home: React.FC<RouteComponentProps> = (props) => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Check Lists</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Check List</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          <IonItem>
+            <IonCheckbox slot="start"/>
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run Idea by Matt</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              5 Days
+            </IonBadge>
+          </IonItem>
+        </IonList>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={ () => props.history.push('/new') }>
+            <IonIcon icon={add} />
+          </IonFabButton>          
+        </IonFab>
       </IonContent>
     </IonPage>
   );
